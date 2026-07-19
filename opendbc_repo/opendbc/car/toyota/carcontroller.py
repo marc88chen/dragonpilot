fc1a269981e14bc5f49380116011147ae255c089
marc88chen <66193562+marc88chen@users.noreply.github.com>
2026-07-19 16:05:33 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -47,8 +47,8 @@ DRIVE = car.CarState.GearShifter.drive
 
 def get_long_tune(CP, params):
   if CP.carFingerprint in TSS2_CAR:
-    kiBP = [2., 5.]
-    kiV = [0.5, 0.25]
+    kiBP = [0.,  2.,  5.,   27.]
+    kiV = [0.33, 0.48, 0.24, 0.1]
   else:
     kiBP = [0., 5., 35.]
     kiV = [3.6, 2.4, 1.5]
