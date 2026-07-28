58786a95f428ec106412471d831f841950701dd9
marc88chen <66193562+marc88chen@users.noreply.github.com>
2026-07-28 19:10:32 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -49,7 +49,7 @@ def get_long_tune(CP, params):
   if CP.carFingerprint in TSS2_CAR:
     #kiBP = [0.,  2.,   5.,   14.,  27.]
     kiBP = [0.2,  1.,  3.,   5.,   14.,  27.]
-    kiV = [0.33, 0.42, 0.42, 0.24, 0.20, 0.12]
+    kiV = [0.33, 0.42, 0.42, 0.244, 0.204, 0.12]
   else:
     kiBP = [0., 5., 35.]
     kiV = [3.6, 2.4, 1.5]
